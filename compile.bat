071e996ac689bb2cb88c4cb5fc55532c0896e294
Angelo Frasca Caccia <98479572+lem0nSec@users.noreply.github.com>
2023-01-07 15:36:45 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,10 @@
 @echo off
 
-nasm -f win64 src/Win64.Alcatraz.asm -o alcatraz.obj 2>NUL
-golink /entry _saveEntryPoint /console alcatraz.obj kernel32.dll
+set program=alcatraz
+
+echo "IMPORTANT: Remember to edit the first program instruction in accordance with the comment in this file"
+echo:
+nasm -f win64 Win64.Alcatraz.asm -o %program%.obj 2>NUL
+golink /entry _saveEntryPoint /console %program%.obj kernel32.dll
+
+Rem IMPORTANT: the first instruction of alcatraz.exe (lea rbx, [rax]) needs to be edited to 'lea rbx, [rip]'
